c5009319ef623b9ba246e727ab673799965c9b16
sakasif <sak_asif@live.com>
2015-10-26 20:47:38 +0400

    changes

--- Markup/public/ui/js/uicreep.js
+++ Markup/public/ui/js/uicreep.js
@@ -30,15 +30,13 @@ var galleryTop = new Swiper('.bannerMainImgSection', {
 
 
 var galleryThumbs = new Swiper('.bannerThumbImgSection', {
-    // spaceBetween: 10,
-    //centeredSlides: true,
-    // slidesPerView: '6',
+    spaceBetween: 10,
+    centeredSlides: false,
+    slidesPerView: '6',
     touchRatio: 0.2,
     slideToClickedSlide: true,
-    slidesPerView: 6,
-    paginationClickable: true,
-    spaceBetween: 10,
-    freeMode: true
+    paginationClickable: false,
+    freeMode: false
 });
 
 galleryTop.params.control = galleryThumbs;
